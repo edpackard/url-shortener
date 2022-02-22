a8e2356a0133b4e2fa5f3139ccfb6ac12b103058
edpackard <edpackard81@gmail.com>
2022-02-22 09:40:48 +0000

    Redirect to destination url when it exists in db

--- src/controllers/ShortenerController.ts
+++ src/controllers/ShortenerController.ts
@@ -1,8 +1,18 @@
-class ShortenerController {
-  async redirectUrl (req: any, res: any) {
-    return res.status(400).json({ message: 'shortId is not provided'})
-  }
+import { Request, Response } from 'express'
+import UrlModel from '../models/UrlModel';
 
+class ShortenerController {
+  async redirectUrl (req: Request, res: Response) {
+    const { shortId } = req.params;
+    if (!shortId) {
+      return res.status(400).json({ message: 'shortId is not provided'})
+    }
+
+    const record = await UrlModel.findOne({ shortId });
+    if (record) {
+      return res.redirect(record.url);
+    }
+  }
   async shortenUrl (req: any, res:any) {
     throw new Error('Not implemented yet!');
   }
