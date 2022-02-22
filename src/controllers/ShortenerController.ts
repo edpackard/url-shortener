9e1abf3f144312f9d348a09d5cc340cfc029b0f0
edpackard <edpackard81@gmail.com>
2022-02-22 10:09:20 +0000

    Finish redirectUrl functionality

--- src/controllers/ShortenerController.ts
+++ src/controllers/ShortenerController.ts
@@ -4,17 +4,25 @@ import UrlModel from '../models/UrlModel';
 class ShortenerController {
   async redirectUrl (req: Request, res: Response) {
     const { shortId } = req.params;
+    
     if (!shortId) {
       return res.status(400).json({ message: 'shortId is not provided'})
     }
 
-    const record = await UrlModel.findOne({ shortId });
-    if (record) {
-      return res.redirect(record.url);
+    try {
+      const record = await UrlModel.findOne({ shortId });
+      if (record) {
+        return res.redirect(record.url);
+      }
+      return res.status(400).json({ message: 'shortId is invalid' })
+    } catch (error) {
+      return res.status(500).json( {message: 'Something went wrong' })
     }
   }
+
+
   async shortenUrl (req: any, res:any) {
-    throw new Error('Not implemented yet!');
+    return res.status(400)
   }
 }
 export default new ShortenerController();
